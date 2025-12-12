05a678acb9f883e7a9b30e566b2020e79e74ce20
Nazreen998 <nareenbegam1998@gmail.com>
2025-12-12 23:00:39 +0530

    Updated

--- models/Counter_temp.js
+++ models/Counter_temp.js
@@ -1,9 +1,15 @@
-// models/Counter.js
 const mongoose = require("mongoose");
 
 const counterSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  seq: { type: Number, default: 0 },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  seq: {
+    type: Number,
+    default: 0,
+  },
 });
 
 module.exports = mongoose.model("Counter", counterSchema);
