8f9d6c474f865135d162d2651f10500ff9d77a90
Nazreen998 <nareenbegam1998@gmail.com>
2025-12-12 11:37:48 +0530

    shop list page

--- models/User.js
+++ models/User.js
@@ -1,19 +1,36 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    user_id: { type: String, required: true },
-    name: { type: String, required: true },
-    mobile: { type: String, required: true },  
-    password: { type: String, required: true }, 
-    role: {
-        type: String,
-        enum: ["master", "manager", "salesman"],
-        required: true
+const userSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: String,
+      required: true,
     },
-    segment: { type: String, required: true }
-}, 
-{
-    timestamps: true, // ✅ THIS IS THE KEY
+    name: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["master", "manager", "salesman"],
+      required: true,
+    },
+    segment: {
+      type: String,
+      required: true,
+    },
   },
+  {
+    timestamps: true, // ✅ createdAt & updatedAt auto
+  }
+);
 
-module.exports = mongoose.model("User", userSchema));
+module.exports = mongoose.model("User", userSchema);
