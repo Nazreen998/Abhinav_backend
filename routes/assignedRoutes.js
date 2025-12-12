8242177e2180569ded616a7b6c1e24399d2e3810
Nazreen998 <nareenbegam1998@gmail.com>
2025-12-12 15:53:53 +0530

    Updated

--- routes/assignedRoutes.js
+++ routes/assignedRoutes.js
@@ -2,19 +2,35 @@ const router = require("express").Router();
 const ctrl = require("../controllers/assignedController");
 const auth = require("../middleware/auth");
 
-// assign
-router.post("/assign", auth(["master", "manager"]), ctrl.assignShop);
+// ASSIGN
+router.post(
+  "/assign",
+  auth(["master", "manager"]),
+  ctrl.assignShop
+);
 
-// list
-router.get("/list", auth(["master", "manager", "salesman"]), ctrl.getAssignedShops);
+// LIST
+router.get(
+  "/list",
+  auth(["master", "manager", "salesman"]),
+  ctrl.getAssignedShops
+);
 
-// remove
-router.post("/remove", auth(["master", "manager"]), ctrl.removeAssigned);
+// REMOVE
+router.post(
+  "/remove",
+  auth(["master", "manager"]),
+  ctrl.removeAssigned
+);
 
-// reorder (🔥 FIXED)
-router.post("/reorder", auth(["master", "manager"]), ctrl.reorderAssignedShops);
+// REORDER
+router.post(
+  "/reorder",
+  auth(["master", "manager"]),
+  ctrl.reorderAssignedShops
+);
 
-// salesman today
+// SALESMAN TODAY
 router.get(
   "/salesman/today",
   auth(["salesman"]),
