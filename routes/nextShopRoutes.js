0ef9728f55e38829fc14da65843247cca696b27a
Nazreen998 <nareenbegam1998@gmail.com>
2025-12-13 11:24:22 +0530

    Updated

--- routes/nextShopRoutes.js
+++ routes/nextShopRoutes.js
@@ -3,7 +3,6 @@ const ctrl = require("../controllers/nextShopController");
 const auth = require("../middleware/auth");
 const uploadMatchImage = require("../middleware/uploadMatchImage");
 
-// 🔥 FRONTEND COMPATIBLE ROUTE
 // Flutter: /api/assign/next/ABHI002
 router.get(
   "/next/:salesmanCode",
@@ -11,7 +10,6 @@ router.get(
   ctrl.getNextShop
 );
 
-// Match shop
 router.post(
   "/match",
   auth(["salesman"]),
