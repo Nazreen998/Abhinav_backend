e03a7cd3d3ef08b8ca22ecbe1812f4df1b62679a
Nazreen998 <nareenbegam1998@gmail.com>
2025-12-13 13:31:05 +0530

    Match Page

--- models/History.js
+++ models/History.js
@@ -1,19 +1,29 @@
 const mongoose = require("mongoose");
 
-const historySchema = new mongoose.Schema({
+const historySchema = new mongoose.Schema(
+  {
     shopName: { type: String, required: true },
     salesmanName: { type: String, required: true },
-    area: { type: String, required: true },
+
+    segment: { type: String, required: true },
+    address: { type: String, required: true },
 
     shopLat: Number,
     shopLng: Number,
     salesmanLat: Number,
     salesmanLng: Number,
 
-    distance: Number,
-    matchStatus: { type: String, enum: ["SUCCESS", "FAILED"], required: true },
+    distance: { type: Number, required: true },
 
-    matchImage: { type: String }, // uploads/history
-}, { timestamps: true });
+    matchStatus: {
+      type: String,
+      enum: ["SUCCESS", "FAILED"],
+      required: true,
+    },
+
+    matchImage: { type: String, required: true }, // uploads/...
+  },
+  { timestamps: true } // ✅ createdAt, updatedAt
+);
 
 module.exports = mongoose.model("History", historySchema);
