7856fec53ba76b8792950b7ad42832e92567d0af
Nazreen998 <nareenbegam1998@gmail.com>
2025-12-23 11:30:14 +0530

    Updated

--- routes/nextShopRoutes.js
+++ routes/nextShopRoutes.js
@@ -1,20 +1,12 @@
 const router = require("express").Router();
 const ctrl = require("../controllers/nextShopController");
 const auth = require("../middleware/auth");
-const uploadMatchImage = require("../middleware/uploadMatchImage");
 
-// Flutter: /api/assign/next/ABHI002
+// Flutter: /api/nextshop/next/:salesmanCode
 router.get(
   "/next/:salesmanCode",
   auth(["salesman"]),
   ctrl.getNextShop
-);
-
-router.post(
-  "/match",
-  auth(["salesman"]),
-  uploadMatchImage,
-  ctrl.matchShop
 );
 
 module.exports = router;
