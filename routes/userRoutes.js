33e96a51071d7f79ad934b0d9442d90263ce4e18
Nazreen998 <nareenbegam1998@gmail.com>
2026-02-18 07:38:18 +0530

    Updated user list-edit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,16 @@ const router = require("express").Router();
 const userController = require("../controllers/userController");
 const { verifyMaster } = require("../middleware/authMiddleware");
 
+// LOGIN
 router.post("/login", userController.login);
-router.post("/add", verifyMaster, userController.addUser);
-router.get("/all", verifyMaster, userController.getAllUsers);
-router.put("/update/:id", verifyMaster, userController.updateUser);
+
+// ADD USER
+router.post("/add", userController.addUser);
+
+// GET ALL USERS
+router.get("/", userController.getAllUsers);
+
+// ✅ UPDATE USER
+router.put("/:id", userController.updateUser);
 
 module.exports = router;
