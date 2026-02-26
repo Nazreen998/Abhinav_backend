f592f80be860ff142c6dc670ea867a97c2c9a4d5
Nazreen998 <nareenbegam1998@gmail.com>
2026-02-26 15:09:33 +0530

    add new user by master

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,20 +1,21 @@
 const jwt = require("jsonwebtoken");
 
 exports.verifyMaster = (req, res, next) => {
-    try {
-        const token = req.headers.authorization?.split(" ")[1];
-        if (!token) return res.status(401).json({ message: "No token" });
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) return res.status(401).json({ message: "No token" });
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        if (decoded.role !== "master") {
-            return res.status(403).json({ message: "Access denied. Master only." });
-        }
-
-        req.user = decoded;
-        next();
-
-    } catch (e) {
-        return res.status(401).json({ message: "Invalid token" });
+    // ✅ FIXED CASE
+    if (decoded.role !== "MASTER") {
+      return res.status(403).json({ message: "Access denied. Master only." });
     }
+
+    req.user = decoded;
+    next();
+
+  } catch (e) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 };
