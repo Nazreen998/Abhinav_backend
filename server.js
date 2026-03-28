905a22b552483521390f28b04972cfa1c867080e
Nazreen998 <nareenbegam1998@gmail.com>
2026-03-28 16:29:19 +0530

    SQL

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
+// ✅ ADD THIS
+const { sql, connectSQL } = require("./db-sql");
 // ROUTES
 const userRoutes = require("./routes/userRoutes");
 const shopRoutes = require("./routes/shopRoutes");
@@ -11,9 +12,7 @@ const nextShopRoutes = require("./routes/nextShopRoutes");
 const historyRoutes = require("./routes/historyRoutes");
 const visitRoutes = require("./routes/visitRoutes");
 const pendingRoutes = require("./routes/pendingRoutes");
-
 const app = express();
-
 // =======================
 // MIDDLEWARE
 // =======================
@@ -21,6 +20,20 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
+app.get("/api/test-sql", async (req, res) => {
+  try {
+    const result = await sql.query("SELECT GETDATE() AS time");
+    res.json({
+      success: true,
+      data: result.recordset,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message,
+    });
+  }
+});
 // =======================
 // STATIC FILES (🔥 VERY IMPORTANT)
 // =======================
