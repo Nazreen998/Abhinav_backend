33f29bf4b9390cbb66003c85631cb090403f67bf
Nazreen998 <nareenbegam1998@gmail.com>
2026-04-25 15:08:28 +0530

    updated history to driver

--- routes/historyRoutes.js
+++ routes/historyRoutes.js
@@ -4,7 +4,7 @@ const auth = require("../middleware/auth");
 
 // Master & Manager → see all
 // Salesman → see own history only
-router.get("/list", auth(["master", "manager", "salesman"]), historyController.getHistory);
+router.get("/list", auth(["master", "manager", "salesman","driver"]), historyController.getHistory);
 
 // Master & Manager
 router.get(
